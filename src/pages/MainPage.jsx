b95fc5190ec9e7ae33510bd49aed8e695152e09f
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-18 17:35:41 -0300

    feat: creates main page component

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Header from '../components/Header';
+import Body from '../components/Body';
+
+export default function MainPage() {
+  return (
+    <>
+      <header><Header /></header>
+      <div><Body /></div>
+    </>
+  );
+}
