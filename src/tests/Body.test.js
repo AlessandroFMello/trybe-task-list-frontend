5c76aa474861b690845931695bb627fa57d74b51
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-05-20 11:02:09 -0300

    fix: removes useless test

--- src/tests/Body.test.js
+++ src/tests/Body.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Enzyme, { mount, Adapter } from 'enzyme';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../renderWithRouter';
 import Body from '../components/Body';
-Enzyme.configure({adapter: new Adapter()})
 
 beforeEach(() => {
   const { history } = renderWithRouter(<Body />);
@@ -16,10 +14,5 @@ describe('Testa o componente <Body />', () => {
   it('Testa se há uma lista não ordenada', () => {
     const list = screen.getByRole('list');
     expect(list).toBeInTheDocument();
-  });
-
-  it('', () => {
-    const wrapper = mount(<Body />);
-   expect(wrapper.find('ul').children()).toBeInTheDocument();
   });
 });
